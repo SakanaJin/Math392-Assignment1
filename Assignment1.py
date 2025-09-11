259aa3b5a05ffcbc2bd77ddb76a49691b1ddc61c
Devin Ford <Devin.Ford4263@gmail.com>
2025-09-11 12:54:42 -0500

    done

--- Assignment1.py
+++ Assignment1.py
@@ -1,16 +1,17 @@
-import math
-import matplotlib.pyplot as plt
+import math # for the math
+import matplotlib.pyplot as plt # for the graphs
+from prettytable import PrettyTable # for some print formatting
 
 TOLERANCE = 1e-5
 MAXITER = 20
 
-def f(x) -> float:
+def f(x) -> float: # given function
     return math.exp(x) + 2**-x + math.cos(x) - 6
 
-def fprime(x) -> float:
+def fprime(x) -> float: # derivative of given function
     return math.exp(x) - (2**-x) * math.log(2) - math.sin(x)
 
-def Newton(p0: float) -> dict:
+def Newton(p0: float) -> dict: # Newton's Method
     toreturn = {"last": {"value": None, "iteration": None, "error": None}}
     for i in range(MAXITER + 1):
         temp = p0 - f(p0)/fprime(p0)
@@ -23,7 +24,7 @@ def Newton(p0: float) -> dict:
     else:
         return None
 
-def Secant(p0: float, p1: float) -> dict:
+def Secant(p0: float, p1: float) -> dict: # Secant Method
     toreturn = {'last': {'value': None, 'iteration': None, 'error': None}}
     fp0 = f(p0)
     fp1 = f(p1)
@@ -41,7 +42,7 @@ def Secant(p0: float, p1: float) -> dict:
     else:
         return None
 
-def Falsepos(p0: float, p1: float) -> dict:
+def Falsepos(p0: float, p1: float) -> dict: # False Position Method
     toreturn = {'last': {'value': None, 'iteration': None, 'error': None}}
     fp0 = f(p0)
     fp1 = f(p1)
@@ -62,21 +63,93 @@ def Falsepos(p0: float, p1: float) -> dict:
         return None
 
 if __name__ == '__main__':
+    # given starting points
     p0 = 1.3
     p1 = 2
 
+    # values for graphing f(x)
     x_vals = list(range(-5, 6))
     y_vals = [f(x) for x in x_vals]
 
-    plt.plot(x_vals, y_vals)
-    plt.plot(x_vals, [0 for _ in x_vals])
+    # initializing subplots and putting f(x) on them
+    fig, axs = plt.subplots(1,3, figsize=(15,4))
+    for ax in axs:
+        ax.plot(x_vals, y_vals)
+        ax.plot(x_vals, [0 for _ in x_vals])
 
+    # run methods and create accepted value
     newtondict = Newton(p0)
     secantdict = Secant(p0, p1)
     falsedict = Falsepos(p0, p1)
+    accepted_value = newtondict['last']['value']
 
-    plt.plot(newtondict['last']['value'], 0, 'ro')
-    plt.annotate(f'({round(newtondict['last']['value'], 6)}, 0)', xy=(newtondict['last']['value'], 0), xytext=(newtondict['last']['value'] + 0.1, 0.5))
+    # plot estimated points on graphs
+    axs[0].set_title("Newton Method")
+    axs[0].plot(accepted_value, 0, 'ro')
+    axs[0].annotate(f'({round(accepted_value, 6)}, 0)', xy=(accepted_value, 0), xytext=(accepted_value + 0.1, 0.5))
+
+    axs[1].set_title("Secant Method")
+    axs[1].plot(secantdict['last']['value'], 0, 'ro')
+    axs[1].annotate(f'({round(secantdict['last']['value'], 6)}, 0)', xy=(secantdict['last']['value'], 0), xytext=(secantdict['last']['value'] + 0.1, 0.5))
+
+    axs[2].set_title("False Position Method")
+    axs[2].plot(falsedict['last']['value'], 0, 'ro')
+    axs[2].annotate(f'({round(falsedict['last']['value'], 6)}, 0)', xy=(falsedict['last']['value'], 0), xytext=(falsedict['last']['value'] + 0.1, 0.5))
+
+    # create tables from data
+    field_names = ["Value", "Iteration", "Error"]
+
+    newtontable = PrettyTable()
+    newtontable.field_names = field_names
+    newtontable.add_rows([
+        [newtondict['secondlast']['value'], newtondict['secondlast']['iteration'], newtondict['secondlast']['error']],
+        [newtondict['last']['value'], newtondict['last']['iteration'], newtondict['last']['error']]
+    ])
+
+    secanttable = PrettyTable()
+    secanttable.field_names = field_names
+    secanttable.add_rows([
+        [secantdict['secondlast']['value'], secantdict['secondlast']['iteration'], secantdict['secondlast']['error']],
+        [secantdict['last']['value'], secantdict['last']['iteration'], secantdict['last']['error']]
+    ])
+
+    falsetable = PrettyTable()
+    falsetable.field_names = field_names
+    falsetable.add_rows([
+        [falsedict['secondlast']['value'], falsedict['secondlast']['iteration'], falsedict['secondlast']['error']],
+        [falsedict['last']['value'], falsedict['last']['iteration'], falsedict['last']['error']]
+    ])
+
+    #display information
+    print("Newton's Method:")
+    print(newtontable)
+    newtonrelerr = abs(accepted_value - newtondict['last']['value'])
+    print(f'Relative Error: {newtonrelerr}\tAbsolute Error: {newtonrelerr/abs(accepted_value)}')
+
+    print()
+
+    print("Secant Method:")
+    print(secanttable)
+    secantrelerr = abs(accepted_value - secantdict['last']['value'])
+    print(f'Relative Error: {secantrelerr}\tAbsolute Error: {secantrelerr/abs(accepted_value)}')
+
+    print()
+
+    print("False Position Method:")
+    print(falsetable)
+    falserelerr = abs(accepted_value - falsedict['last']['value'])
+    print(f'Relative Error: {falserelerr}\tAbsolute Error: {falserelerr/abs(accepted_value)}')
+
+    print()
+
     plt.show()
 
-    #print(f'newton: {newtondict}\nsecant: {secantdict}\nfalse: {falsedict}')
+    # written response--------------------------------------------------------------------------------------------
+    # Newton's and Secant took the same amount of iterations with 4 iterations, and False Position took 6.
+    # Using Newton's to get the accepted value automatically gives the method a relative and absolue error of 0.
+    # False Position has a higher relative and absolute error than Secant Method.
+    # It's hard to say wheter Newton's or Secant are better than the other since they have the same number of iterations 
+    # and we're using Newton's as the accepted value, but for this problem it seems False Position is definitely the worst method to use.
+    # If we had a different accepted value this might would change, but it has the biggest errors and the most iterations.
+    # It's also the most complex of the methods. Newton's is the simplest, but it requires an exact derivative where the Secant and False Positioning
+    # approximates both.
---
+++ readme.txt
@@ -0,0 +1,19 @@
+If you run this code in colab you should just be able to copy and paste and not have to pip install anything and have it run fine.
+
+If you're trying to run the program on your machine here are some instructions for windows (I recommend command prompt over powershell):
+
+If you dont want a virtual environment you can skip steps 1 and 4
+
+1.) virtual environment (if you care about that)-
+    python -m venv .venv
+    .\.venv\Scripts\activate.bat (command prompt)
+    .\.venv\Scripts\Activate.ps1 (powershell)
+
+2.) install requirements-
+    pip install -r requirements.txt
+
+3.) Running-
+    python Assignment1.py
+
+4.) Deactivate virtual environment-
+    deactivate 
